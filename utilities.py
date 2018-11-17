d80f2f6a1eeedf4657c5c52600755927ed5dab4e
garrettdimick <garrett.dimick@gmail.com>
2018-11-17 00:29:35 -0700

    from png to jpg

--- utilities.py
+++ utilities.py
@@ -9,7 +9,7 @@ def rename_files(filepath):
     f = glob.glob(filepath)
     # print(f)
     for i, file in enumerate(f):
-        newname = filepath[:-1] + prefix + str(i) + '.png'
+        newname = filepath[:-1] + prefix + str(i) + '.jpg'
         try:
             newfile = open(file, "r+")
             os.rename(file, newname)
