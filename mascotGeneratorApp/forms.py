3c0eb698add40bde30fc559ef912ca426a621221
root <root@ip-172-31-17-240.us-west-2.compute.internal>
2018-11-17 11:56:45 -0700

    placed beginnings of sticky header functionality.

--- mascotGeneratorApp/forms.py
+++ mascotGeneratorApp/forms.py
@@ -2,8 +2,8 @@ from django import forms
 
 class GeneratorForm(forms.Form):
     CHOICES = (('Cat', 'Cat'),('Dragon', 'Dragon'),('Turtle','Turtle'),('Octopus','Octopus'))
-    what_is_your_name = forms.CharField(max_length=100)(required=false)
-    what_is_your_quest = forms.CharField(max_length=100)
-    what_is_the_air_speed_velocity_of_an_unladen_swallow = forms.CharField(max_length=100)
+    what_is_your_name = forms.CharField(max_length=100, required=False)
+    what_is_your_quest = forms.CharField(max_length=100, required=False)
+    what_is_the_air_speed_velocity_of_an_unladen_swallow = forms.CharField(max_length=100, required=False)
     desired_animal = forms.ChoiceField(choices=CHOICES)
     
